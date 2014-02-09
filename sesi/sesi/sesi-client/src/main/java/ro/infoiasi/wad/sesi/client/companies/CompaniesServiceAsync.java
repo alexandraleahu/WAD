4bf81e9a2f74584a5d4bc9bf7b8bb77203d1e2b0
alexandraleahu <l.allexandra@gmail.com>
2014-02-09 22:29:16 +0200

    companies view and editor

---
+++ sesi/sesi/sesi-client/src/main/java/ro/infoiasi/wad/sesi/client/companies/CompaniesServiceAsync.java
@@ -0,0 +1,26 @@
+package ro.infoiasi.wad.sesi.client.companies;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import ro.infoiasi.wad.sesi.core.model.Company;
+import ro.infoiasi.wad.sesi.core.model.Internship;
+import ro.infoiasi.wad.sesi.core.model.InternshipApplication;
+import ro.infoiasi.wad.sesi.core.model.InternshipProgressDetails;
+
+import java.util.List;
+
+public interface CompaniesServiceAsync {
+    void getCompanyById(String companyId, AsyncCallback<Company> async);
+
+    void getCompanyApplications(String companyApplications, AsyncCallback<List<InternshipApplication>> async);
+
+    void getAllCompanies(AsyncCallback<List<Company>> async);
+
+    void getCompanyInternships(String companyId, AsyncCallback<List<Internship>> async);
+
+    void getCompanyProgressDetails(String companyId, AsyncCallback<List<InternshipProgressDetails>> async);
+
+    void getAllCompaniesNames(AsyncCallback<List<String>> async);
+
+    void registerCompany(String username, String password, String name, AsyncCallback<Boolean> async);
+
+}
